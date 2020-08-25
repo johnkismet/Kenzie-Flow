9e087e3be5ffb96ad78d08f98f50965cd22453c7
johnkismet <mojomanftw@gmail.com>
2020-08-25 19:43:51 -0400

    Improved functionality for the flow timer

--- script.js
+++ script.js
@@ -1,119 +1,137 @@
 // Global variables
 let flowTime = (document.getElementById('flowTimer').innerHTML = 00 + ':' + '03');
-let shortTime = (document.getElementById('shortTimer').innerHTML = 00 + ':' + '03');
-let longTime = (document.getElementById('longTimer').innerHTML = 00 + ':' + '03');
+let shortTime = (document.getElementById('shortTimer').innerHTML = 5 + ':' + '03');
+let longTime = (document.getElementById('longTimer').innerHTML = 15 + ':' + '03');
 
 const alarm = document.getElementById('alarm');
 let flowCount = 0;
 let paused = false;
-let hasPlayed = false;
 var started = false;
+let played = false;
 
 // Functions
 function unpause() {
 	paused = false;
 }
 function reset() {
-	let flowButton = document.getElementById('flowPauseBtn');
-	flowButton.innerHTML = 'Pause';
-	let shortButton = document.getElementById('button2');
-	shortButton.innerHTML = 'Pause';
-	let longButton = document.getElementById('button3');
-	longButton.innerHTML = 'Pause';
-	let flow = document.getElementById('p1');
-	flow.innerHTML = '';
-	let sbreak = document.getElementById('p2');
-	sbreak.innerHTML = '';
-	let lbreak = document.getElementById('p3');
-	lbreak.innerHTML = '';
-	// let flowTime = document.getElementById('flowTimer');
-	// flowTime.innerHTML = 00 + ':' + '03';
-	// let shortTime = document.getElementById('shortTimer');
-	// shortTime.innerHTML = 00 + ':' + '03';
-	// let longTime = document.getElementById('longTimer');
-	// longTime.innerHTML = 00 + ':' + '03';
-	hasPlayed = false;
+	started = false;
+	paused = false;
+	// set the pause buttons to no display
+	document.getElementById('flowPauseBtn').style.display = 'none';
+	document.getElementById('shortPauseBtn').style.display = 'none';
+	document.getElementById('longPauseBtn').style.display = 'none';
+
+	// set the start buttons to display
+	document.getElementById('flowStartBtn').style.display = '';
+	document.getElementById('shortBox').style.display = '';
+	document.getElementById('longBox').style.display = '';
+
 	document.getElementById('title').innerHTML = 'Kenzie Flow';
-	document.getElementById('flowTimer').style.display = '';
+
+	// set the boxes to have the correct time
+
+	// flow
+	document.getElementById('flowh4').style.display = 'none';
 	document.getElementById('timerDiv').style.display = '';
-	document.getElementById('shortTimer').style.display = '';
+	document.getElementById('flowTimer').style.display = '';
+	// short
+	document.getElementById('shorth4').style.display = 'none';
 	document.getElementById('stimerDiv').style.display = '';
-	document.getElementById('longTimer').style.display = '';
+	document.getElementById('shortTimer').style.display = '';
+	// long
+	document.getElementById('longh4').style.display = 'none';
 	document.getElementById('ltimerDiv').style.display = '';
+	document.getElementById('longTimer').style.display = '';
 }
+
+function displayStart() {}
+function displayPause() {}
 
 function flowStart() {
 	if (paused === true) {
 		return;
 	}
-
-	if (hasPlayed === false) {
-		startTimer();
-		hasPlayed = true;
-	}
-	started = true;
-	var active = document.getElementById('flow');
-	var active1 = document.getElementById('short');
-	var active2 = document.getElementById('long');
-
-	active.style.background = '#4a75ee';
-	active1.style.display = 'none';
-	active2.style.display = 'none';
 	document.getElementById('flowStartBtn').style.display = 'none';
+	document.getElementById('flowBox').style.background = '#4a75ee';
+	document.getElementById('shortBox').style.display = 'none';
+	document.getElementById('longBox').style.display = 'none';
 
+	// dynamically create pause button
+	if (started === false && played === false) {
+		var pause = document.createElement('button');
+		pause.setAttribute('id', 'flowPauseBtn');
+		pause.setAttribute('class', 'pause');
+		pause.setAttribute('onclick', 'reset(); flowPause()');
+		pause.innerHTML = 'Pause';
+		document.getElementById('flowBox').appendChild(pause);
+		started = true;
+	} else if (started === false) {
+		document.getElementById('flowPauseBtn').style.display = '';
+	}
+
+	// timer
 	var presentTime = document.getElementById('flowTimer').innerHTML;
 	var timeArray = presentTime.split(/[:]+/);
 	var m = timeArray[0];
 	var s = checkSecond(timeArray[1] - 1);
-
 	if (s == 59) {
 		m = m - 1;
 	}
+
+	// timer done statement
 	if (m < 0) {
 		flowCount++;
 		console.log(`Flow count: ${flowCount}`);
 		soundAlarm();
+
+		// Remove time text in place of the nice message
 		document.getElementById('flowTimer').style.display = 'none';
 		document.getElementById('timerDiv').style.display = 'none';
 
+		// if under 4 > Have a short break, and make the pause button say Go Back
+		// if 4 > Have a long break, and make pause button say Go Back
 		if (flowCount < 4) {
-			let shortBreak = document.getElementById('p1');
-			shortBreak.innerHTML = `Good work! Have a short break :) <br> Flow Cycles: ${flowCount}`;
-			let goBack = document.getElementById('flowPauseBtn');
-			goBack.innerHTML = 'Go back';
-			document.getElementById('title').innerHTML = 'Times up!';
+			document.getElementById(
+				'flowh4'
+			).innerHTML = `Good work! Have a short break :) <br> Flow Cycles: ${flowCount}`;
+			document.getElementById('flowh4').style.display = '';
+			document.getElementById('flowPauseBtn').innerHTML = 'Go back';
+
+			document.getElementById('title').innerHTML = 'Break time!';
 		} else if (flowCount === 4) {
-			let longBreak = document.getElementById('p1');
-			longBreak.innerHTML = `You've done so much! Have a long break <br> Flow Cycles: ${flowCount}`;
-			let goBack = document.getElementById('button');
-			goBack.innerHTML = 'Go back';
-			document.getElementById('title').innerHTML = 'Times up!';
+			document.getElementById(
+				'flowh4'
+			).innerHTML = `You've done so much! Have a long break <br> Flow Cycles: ${flowCount}`;
+			document.getElementById('flowh4').style.display = '';
+
+			document.getElementById('flowPauseBtn').innerHTML = 'Go back';
+			document.getElementById('title').innerHTML = 'Break time!';
 			flowCount = 0;
 		}
 		document.getElementById('flowTimer').innerHTML = flowTime;
 		return;
 	}
 
-	let timeLeft = (document.getElementById('flowTimer').innerHTML = m + ':' + s); // display time remaining
+	// display time remaining in title
+	let timeLeft = (document.getElementById('flowTimer').innerHTML = m + ':' + s);
 
 	setTimeout(flowStart, 1000);
 	document.getElementById('title').innerHTML = 'FLOW ' + timeLeft;
 }
 
 function flowPause() {
-	if (started === false) {
-		console.log('not started!');
-		return;
-	}
 	paused = true;
-	var inactive = document.getElementById('flow');
-	var inactive1 = document.getElementById('short');
-	var inactive2 = document.getElementById('long');
+	played = true;
 
-	inactive.style.background = '#5769c5';
-	inactive1.style.display = '';
-	inactive2.style.display = '';
+	// dynamically creat start button
 
+	document.getElementById('flowPauseBtn').style.display = 'none';
+	document.getElementById('flowStartBtn').style.display = '';
+	document.getElementById('flowBox').style.background = '#4a75ee';
+	document.getElementById('shortBox').style.display = '';
+	document.getElementById('longBox').style.display = '';
+
+	// keep timer consistent
 	var presentTime = document.getElementById('flowTimer').innerHTML;
 	var timeArray = presentTime.split(/[:]+/);
 	var m = timeArray[0];
@@ -125,18 +143,25 @@ function shortStart() {
 	if (paused === true) {
 		return;
 	}
-	if (hasPlayed === false) {
-		startTimer();
-		hasPlayed = true;
+	document.getElementById('shortStartBtn').style.display = 'none';
+	document.getElementById('shortBox').style.background = '#4a75ee';
+	document.getElementById('flowBox').style.display = 'none';
+	document.getElementById('longBox').style.display = 'none';
+
+	// dynamically create pause button
+	if (started === false && played === false) {
+		var pause = document.createElement('button');
+		pause.setAttribute('id', 'shortPauseBtn');
+		pause.setAttribute('class', 'pause');
+		pause.setAttribute('onclick', 'reset(); shortPause()');
+		pause.innerHTML = 'Pause';
+		document.getElementById('shortBox').appendChild(pause);
+		started = true;
+	} else if (started === false) {
+		document.getElementById('shortPauseBtn').style.display = '';
 	}
-	var active = document.getElementById('short');
-	var active1 = document.getElementById('flow');
-	var active2 = document.getElementById('long');
 
-	active.style.background = '#4a75ee';
-	active1.style.display = 'none';
-	active2.style.display = 'none';
-
+	// timer setup
 	var presentTime = document.getElementById('shortTimer').innerHTML;
 	var timeArray = presentTime.split(/[:]+/);
 	var m = timeArray[0];
@@ -147,14 +172,13 @@ function shortStart() {
 	}
 	if (m < 0) {
 		soundAlarm();
-		let goBack = document.getElementById('button2');
-		goBack.innerHTML = 'Go back';
-		let shortBreak = document.getElementById('p2');
-		shortBreak.innerHTML = 'Hope you had a good break! Time to get back into the flow.';
-		document.getElementById('shortTimer').innerHTML = shortTime;
-		document.getElementById('title').innerHTML = 'Times up!';
 		document.getElementById('shortTimer').style.display = 'none';
 		document.getElementById('stimerDiv').style.display = 'none';
+		document.getElementById('shorth4').innerHTML = `Good break! Get back to work!`;
+		document.getElementById('shorth4').style.display = '';
+		document.getElementById('shortPauseBtn').innerHTML = 'Go back';
+
+		document.getElementById('title').innerHTML = 'Times up!';
 		return;
 	}
 
@@ -167,14 +191,17 @@ function shortStart() {
 
 function shortPause() {
 	paused = true;
-	var inactive = document.getElementById('short');
-	var inactive1 = document.getElementById('flow');
-	var inactive2 = document.getElementById('long');
+	played = true;
 
-	inactive.style.background = '#5769c5';
-	inactive1.style.display = '';
-	inactive2.style.display = '';
+	// dynamically creat start button
 
+	document.getElementById('shortPauseBtn').style.display = 'none';
+	document.getElementById('shortStartBtn').style.display = '';
+	document.getElementById('shortBox').style.background = '#4a75ee';
+	document.getElementById('flowBox').style.display = '';
+	document.getElementById('longBox').style.display = '';
+
+	// keep timer consistent
 	var presentTime = document.getElementById('shortTimer').innerHTML;
 	var timeArray = presentTime.split(/[:]+/);
 	var m = timeArray[0];
