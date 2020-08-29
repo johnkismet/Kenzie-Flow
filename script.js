91f66a4871a40e574fc0b15df5da15b6c0f030db
johnkismet <mojomanftw@gmail.com>
2020-08-28 20:44:45 -0400

    added design for future settings menu (not yet functional)

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ let longTime = (document.getElementById('longTimer').innerHTML = 15 + ':' + '00'
 
 // global variables
 let flowCount = 0;
-var paused = false;
-var started = false;
-let played = false;
+let paused = false;
+let started = false;
 let hasPlayed = false;
 
+// settings modal
+// Get the modal
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+	modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
 // default html
 
 document.getElementById('flowPauseBtn').style.display = 'none';
@@ -22,8 +48,6 @@ document.getElementById('flowh2').style.fontSize = '70px';
 document.getElementById('shorth2').style.fontSize = '70px';
 document.getElementById('longh2').style.fontSize = '70px';
 
-// document.getElementById('flowh2').style.font-size = 20;
-
 // functions
 
 function unpause() {
@@ -31,6 +55,10 @@ function unpause() {
 }
 function unstart() {
 	started = false;
+}
+
+function settings() {
+	alert();
 }
 
 function flowReset() {
@@ -45,7 +73,6 @@ function flowReset() {
 	document.getElementById('flowTimer').style.display = 'none';
 	document.getElementById('timerDiv').style.display = 'none';
 	document.getElementById('flowBox').style.background = '';
-
 	hasPlayed = false;
 	document.getElementById('title').innerHTML = 'Kenzie Flow';
 }
