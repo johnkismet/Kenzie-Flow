5ae272bde0b01176cc562737e1f1b47b5de98f91
johnkismet <mojomanftw@gmail.com>
2020-08-31 09:50:09 -0400

    refactored timers slightly

--- script.js
+++ script.js
@@ -9,33 +9,25 @@ let paused = false;
 let started = false;
 let hasPlayed = false;
 
-// settings modal
-// Get the modal
-// Get the modal
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
+// var btn = document.getElementById('myBtn');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+// var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-	modal.style.display = 'block';
-};
+// btn.onclick = function() {
+// 	modal.style.display = 'block';
+// };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-	modal.style.display = 'none';
-};
+// span.onclick = function() {
+// 	modal.style.display = 'none';
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
-};
+// window.onclick = function(event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = 'none';
+// 	}
+// };
 // default html
 
 document.getElementById('flowPauseBtn').style.display = 'none';
@@ -186,7 +178,7 @@ function flowStart() {
 
 function flowPause() {
 	paused = true;
-
+	hasPlayed = false;
 	// style handling
 	document.getElementById('flowStartBtn').style.display = '';
 	document.getElementById('flowPauseBtn').style.display = 'none';
@@ -262,7 +254,7 @@ function shortStart() {
 
 function shortPause() {
 	paused = true;
-
+	hasPlayed = false;
 	// style handling
 	document.getElementById('shortStartBtn').style.display = '';
 	document.getElementById('shortPauseBtn').style.display = 'none';
@@ -338,7 +330,7 @@ function longStart() {
 
 function longPause() {
 	paused = true;
-
+	hasPlayed = false;
 	// style handling
 	document.getElementById('longStartBtn').style.display = '';
 	document.getElementById('longPauseBtn').style.display = 'none';
